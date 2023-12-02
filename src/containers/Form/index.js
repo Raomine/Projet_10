f9ea83067da736f88b5b71ba5600b84ca3bffd78
rao <138585813+Raomine@users.noreply.github.com>
2023-12-02 10:55:19 +0000

    La modal s'affiche à l'envoie du formulaire

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,30 +4,37 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+const mockContactApi = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
+
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
+
       setSending(true);
-      // We try to call mockContactApi
+
       try {
         await mockContactApi();
         setSending(false);
       } catch (err) {
-        setSending(false);
         onError(err);
+        setSending(false);
       }
     },
     [onSuccess, onError]
   );
+
   return (
     <form onSubmit={sendContact}>
       <div className="row">
         <div className="col">
           <Field placeholder="" label="Nom" />
+
           <Field placeholder="" label="Prénom" />
           <Select
             selection={["Personel", "Entreprise"]}
@@ -36,8 +43,19 @@ const Form = ({ onSuccess, onError }) => {
             type="large"
             titleEmpty
           />
-          <Field placeholder="" label="Email" />
-          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
+
+          <Field
+            placeholder=""
+            label="Email"
+            name="Email"
+            type={FIELD_TYPES.InputEmail}
+          />
+          <Button
+            type={BUTTON_TYPES.SUBMIT}
+            disabled={sending}
+            onClick={onSuccess}
+            aria-label="submit btn"
+          >
             {sending ? "En cours" : "Envoyer"}
           </Button>
         </div>
@@ -46,6 +64,7 @@ const Form = ({ onSuccess, onError }) => {
             placeholder="message"
             label="Message"
             type={FIELD_TYPES.TEXTAREA}
+            name="msg"
           />
         </div>
       </div>
@@ -56,11 +75,11 @@ const Form = ({ onSuccess, onError }) => {
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
 Form.defaultProps = {
   onError: () => null,
-  onSuccess: () => null,
-}
+  onSuccess: () => !null,
+};
 
 export default Form;
